d7b0ba34a7b3033fd96b422078fbb9f96b5324f8
Antonio Sousa <antonioufpi@gmail.com>
2023-11-22 21:51:24 -0300

    c1

---
+++ README.md
@@ -0,0 +1,3 @@
+
+link para o figma:
+https://www.figma.com/file/lrYP8sIEJAtKQoycoM9vac/Origin-Six-(Community)?node-id=68%3A131&mode=dev
---
+++ assets/js/oque.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById('menu');
+const closeBtn = document.getElementById('close');
+
+closeBtn.addEventListener('click', (e)=>{
+    menu.style.visibility = 'hidden'
+    // menu.classList.toggle('show');
+})
