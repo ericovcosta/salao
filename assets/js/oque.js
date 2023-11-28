1e4efdfd6eeaafdc17676398e5da62c8be258a7f
ericovcosta <professor@ericocosta.com.br>
2023-11-27 21:55:02 -0300

    hide

--- assets/js/oque.js
+++ assets/js/oque.js
@@ -1,7 +1,11 @@
 const menu = document.getElementById('menu');
-const closeBtn = document.getElementById('close');
+const closeBtn = document.querySelector('.btn_menu');
 
 closeBtn.addEventListener('click', (e)=>{
-    menu.style.visibility = 'hidden'
-    // menu.classList.toggle('show');
+    // menu.style.display = 'None'
+    const iconMenu = document.querySelector('.fa-bars');
+    const iconClose = document.querySelector('.fa-xmark');
+    menu.classList.toggle('hide');
+    iconMenu.classList.toggle('hide');
+    iconClose.classList.toggle('hide');
 })
